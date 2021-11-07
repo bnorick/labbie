3c81a7fca36ef9219ac0c4afc3919a1b6de46f73
Brandon Norick <b.norick@gmail.com>
2021-11-07 01:51:08 -0700

    Reduce logspam

--- src/labbie/result.py
+++ src/labbie/result.py
@@ -10,3 +10,20 @@ class Result:
     search: str
     league_result: Optional[List[enchants.Enchant]]
     daily_result: Optional[List[enchants.Enchant]]
+
+    def __repr__(self):
+        if self.league_result:
+            league_result_repr = [self.league_result[0]]
+            if len(self.league_result) > 1:
+                league_result_repr.append(f'(... {len(self.league_result) - 1} other results ...)')
+        else:
+            league_result_repr = self.league_result
+
+        if self.daily_result:
+            daily_result_repr = [self.daily_result[0]]
+            if len(self.daily_result) > 1:
+                daily_result_repr.append(f'(... {len(self.daily_result) - 1} other results ...)')
+        else:
+            daily_result_repr = self.daily_result
+
+        return f'{type(self).__name__}(title={self.title!r}, search={self.search!r}, league_result={daily_result_repr!r}, daily_result={daily_result_repr!r})'
