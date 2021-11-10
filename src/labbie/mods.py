d0c291525deca0818307ade91c4f85efa1bdd928
Caithir <colter.norick@gmail.com>
2021-11-10 15:28:32 -0800

    Ocr improvements (#5)
    Changed ocr to use config= --psm 12 which has increased accuracy at the cost of reading line formatting correctly. This also enables reading the full image in one go allowing labbie to be flexible to handle different types of labs, and numbers of lab enchants. To counter act the introduced downside the read enchants from the ocr are compared against the complete list of enchants using a trie for prefix matching for efficiency.
    
    Other improvement to the OCR that was introduced is to do the grayscale over all of the provided color channels.
    CV2 was also added to the project, in order to use different morphologies to improve accuracy. First one added is a Dilation pass to thicken the characters. This is turned off unless you modify the constants file.

--- src/labbie/mods.py
+++ src/labbie/mods.py
@@ -1,9 +1,12 @@
 import dataclasses
+from logging import log
+import string
 import functools
 from typing import Dict, List, Optional, Tuple, Union
 
 import injector
 import loguru
+import datrie
 
 from labbie import resources
 from labbie import trade
@@ -107,6 +110,50 @@ class Mods:
         self._fix_krangled_helm_mods(result)
 
         return result
+
+    @functools.cached_property
+    def mod_trie(self) -> datrie.Trie:
+        logger.debug('Creating Trie')
+        trie = datrie.Trie(string.printable)
+        for mod in self.helm_mods:
+            trie[mod] = 1
+        return trie
+
+    def get_mod_list_from_ocr_results(self, enchant_list):
+        trie = self.mod_trie
+        logger.debug(f'Data from OCR{enchant_list}')
+        full_enchants = []
+        potential_enchants = []
+        for partial_enchant in enchant_list:
+            if partial_enchant == '':
+                continue
+            partial_enchant = partial_enchant.replace('’', "'")
+            # continuation of enchant
+            if potential_enchants:
+                is_partial_enchant = False
+                reduced_potential_enchants = []
+                for enchant in potential_enchants:
+                    if partial_enchant in enchant:
+                        reduced_potential_enchants.append(enchant)
+                        if enchant not in full_enchants:
+                            full_enchants.append(enchant)
+                        is_partial_enchant = True
+                potential_enchants = reduced_potential_enchants
+                if is_partial_enchant:
+                    continue
+                # multiple potential enchants still, but the extras will be filtered out
+                # when doing comparison against the trade searches.
+                if len(potential_enchants) > 1:
+                    full_enchants.extend(potential_enchants)
+            potential_enchants = trie.keys(partial_enchant)
+
+            if len(potential_enchants) == 1:
+                full_enchants.extend(potential_enchants)
+
+        if len(potential_enchants) > 1:
+            full_enchants.extend(potential_enchants)
+        logger.info(f'found {full_enchants=}')
+        return full_enchants
 
     def _fix_krangled_helm_mods(self, helm_mod_info: Dict[str, HelmModInfo]):
         helm_mod_info['Fireball Always Ignites'].display = False
