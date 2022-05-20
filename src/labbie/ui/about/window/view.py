afce5cf1e0f1fd035fe4cdd54ebc90b8585eab23
Brandon Norick <b.norick@gmail.com>
2022-05-20 10:49:27 -0700

    Bugfix

--- src/labbie/ui/about/window/view.py
+++ src/labbie/ui/about/window/view.py
@@ -9,7 +9,7 @@ from labbie.ui.about.widget import view as about_widget
 class AboutWindow(base.BaseWindow):
 
     def __init__(self, widget: about_widget.AboutWidget):
-    super().__init__(widget=widget)
+        super().__init__(widget=widget)
         self.set_buttons(close=True)
         flags = self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint
         if os.name == 'posix':
