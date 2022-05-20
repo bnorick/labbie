c8bf454bb8fef5c8d94426444a55341535773aee
Brandon Norick <b.norick@gmail.com>
2022-05-20 10:02:34 -0700

    Update for Sentinel League
    - Updated resources for Sentinel
    - Added simple Linux / MacOS support

--- src/labbie/ui/about/window/view.py
+++ src/labbie/ui/about/window/view.py
@@ -1,3 +1,5 @@
+import os
+
 from PyQt5 import QtCore
 
 from labbie.ui import base
@@ -7,6 +9,9 @@ from labbie.ui.about.widget import view as about_widget
 class AboutWindow(base.BaseWindow):
 
     def __init__(self, widget: about_widget.AboutWidget):
-        super().__init__(widget=widget)
+    super().__init__(widget=widget)
         self.set_buttons(close=True)
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+        flags = self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint
+        if os.name == 'posix':
+            flags = flags | QtCore.Qt.Tool
+        self.setWindowFlags(flags)
--- src/labbie/ui/search/window/view.py
+++ src/labbie/ui/search/window/view.py
@@ -1,3 +1,5 @@
+import os
+
 from PyQt5 import QtCore
 
 from labbie.ui import base
@@ -9,7 +11,13 @@ class SearchWindow(base.BaseWindow):
     def __init__(self, widget: search_widget.SearchWidget):
         super().__init__(widget=widget)
         self.set_buttons(minimize=False, close=True)
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.SubWindow)
+
+        flags = self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint
+        if os.name == 'nt':
+            flags = flags | QtCore.Qt.SubWindow
+        elif os.name == 'posix':
+            flags = flags | QtCore.Qt.Tool
+        self.setWindowFlags(flags)
 
     # NOTE: If we want to add minimize button back, this makes it act like close
     # ref: https://stackoverflow.com/questions/16036336/how-to-change-minimize-event-behavior-in-pyqt-or-pyside
